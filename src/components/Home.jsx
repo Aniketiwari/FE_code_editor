51af4dcc8d0054cad013fd3ad086578f436cf718
Aniketiwari <138387722+Aniketiwari@users.noreply.github.com>
2023-07-14 01:40:17 +0530

    Add files via upload

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,1 +1,18 @@
 
+
+//components
+import Code from './Code';
+import Header from "./Header";
+import Result from "./Result";
+
+const Home = () => {
+    return (
+        <> 
+         <Header />
+         <Code />
+         <Result />
+         </>
+    )
+}
+
+export default Home;
